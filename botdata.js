9cbee29603f5e99435ca21d7c36f5117daf93aca
Adam <allshoez88@gmail.com>
2025-09-10 05:23:00 +0700

    Edit file botdata.js

--- botdata.js
+++ botdata.js
@@ -1,17 +1,7 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('chatbot-cache').then(cache => {
-      return cache.addAll([
-        './index.html',
-        './script.js',
-        './botdata.js',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respond
+let botRules = [
+  { Tanya: ["hai", "halo", "hei"], Jawab: "hai manis... baru baca chat kamu aja udah bikin senyum 😘" },
+  { Tanya: ["lagi apa", "ngapain", "sedang apa"], Jawab: "lagi rebahan... sambil bayangin kamu 😏" },
+  { Tanya: ["udah makan", "sudah makan", "makan belum"], Jawab: "udah sih, tapi rasanya kurang kalau nggak bareng kamu 😋" },
+  { Tanya: ["dimana", "lagi dimana", "posisi dimana"], Jawab: "lagi di kamar... sepi, nunggu ada yang nemenin 😌" },
+  { Tanya: ["sayang aku", "suka aku", "kangen aku"], Jawab: "sayang banget... tiap hari pengennya nempel sama kamu 💕" }
+];
