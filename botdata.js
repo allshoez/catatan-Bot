cbe3035b4039de97ac20fb36cf5d06466e1c0ba9
Adam <allshoez88@gmail.com>
2025-09-12 01:40:25 +0700

    Edit file botdata.js

--- botdata.js
+++ botdata.js
@@ -1,7 +1,7 @@
 let botRules = [
-  { Tanya: ["hai", "halo", "hei"], Jawab: "hai manis... baru baca chat kamu aja udah bikin senyum ð" },
-  { Tanya: ["lagi apa", "ngapain", "sedang apa"], Jawab: "lagi rebahan... sambil bayangin kamu ð" },
-  { Tanya: ["udah makan", "sudah makan", "makan belum"], Jawab: "udah sih, tapi rasanya kurang kalau nggak bareng kamu ð" },
-  { Tanya: ["dimana", "lagi dimana", "posisi dimana"], Jawab: "lagi di kamar... sepi, nunggu ada yang nemenin ð" },
-  { Tanya: ["sayang aku", "suka aku", "kangen aku"], Jawab: "sayang banget... tiap hari pengennya nempel sama kamu ð" }
+  { Tanya: ["hai", "halo", "hei"], Jawab: "hai manis... baru baca chat kamu aja udah bikin senyum 😃" },
+  { Tanya: ["lagi apa", "ngapain", "sedang apa"], Jawab: "lagi rebahan... sambil bayangin kamu 😃" },
+  { Tanya: ["udah makan", "sudah makan", "makan belum"], Jawab: "udah sih, tapi rasanya kurang kalau nggak bareng kamu 😃" },
+  { Tanya: ["dimana", "lagi dimana", "posisi dimana"], Jawab: "lagi di kamar... sepi, nunggu ada yang nemenin 😃" },
+  { Tanya: ["sayang aku", "suka aku", "kangen aku"], Jawab: "sayang banget... tiap hari pengennya nempel sama kamu 😃" }
 ];
